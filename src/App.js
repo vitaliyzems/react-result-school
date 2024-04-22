7e025c9bb83e112ab4c2825aa1d0fa11f7809bbc
Vitaliy <vitalyazems@gmail.com>
2024-04-22 18:49:35 +0300

    lesson-1 task-2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	const date = new Date(); // - императивный подход
-	// Здесь начинается декларативный подход
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<br />
-				{date.getFullYear()}
-			</header>
-		</div>
+	const date = new Date();
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('br'),
+			`${date.getFullYear()}`,
+		),
 	);
 };

cd796040ab7cf20f9801802ec38a6fd8aa8d5426
Vitaliy <vitalyazems@gmail.com>
2024-05-18 23:51:59 +0500

    empty template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,5 @@
-import { createElement } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 export const App = () => {
-	const date = new Date();
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement(
-				'p',
-				null,
-				'Edit ',
-				createElement('code', null, 'src/App.js'),
-				' and save to reload.',
-			),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			createElement('br'),
-			`${date.getFullYear()}`,
-		),
-	);
+	return <></>;
 };

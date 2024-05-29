34e9887642f9b0d0112bb7d61a82db3ecfc46e42
Vitaliy <vitalyazems@gmail.com>
2024-05-29 13:59:24 +0500

    lesson 4, task 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,10 @@
+import { Form } from '../Form/Form';
 import styles from './App.module.css';
 
 export const App = () => {
-	return <></>;
+	return (
+		<div className={styles.app}>
+			<Form />
+		</div>
+	);
 };
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,134 @@
+import { useForm } from '../../hooks/useForm';
+import { useFormError } from '../../hooks/useFormError';
+import { checkMaxLength, checkMinLength, testEmail } from '../../utils/validate';
+import styles from './Form.module.css';
+
+const sendForm = (formData) => {
+	console.log(formData);
+};
+
+export const Form = () => {
+	const { getFormData, updateFormData } = useForm();
+	const { getFormError, updateFormError } = useFormError();
+
+	const { email, password, confirmPassword } = getFormData();
+	const { emailError, passwordError, confirmPasswordError } = getFormError();
+
+	const onEmailChange = ({ target }) => {
+		updateFormData('email', target.value);
+
+		if (testEmail(target.value)) {
+			updateFormError('emailError', null);
+		}
+	};
+
+	const onEmailBlur = ({ target }) => {
+		let newError = null;
+
+		if (!testEmail(target.value)) {
+			newError = 'Введите корректный Email';
+		}
+
+		updateFormError('emailError', newError);
+	};
+
+	const onPasswordChange = ({ target }) => {
+		updateFormData('password', target.value);
+		updateFormData('confirmPassword', '');
+		updateFormError('confirmPasswordError', null);
+
+		let newError = null;
+
+		if (!checkMaxLength(target.value, 20)) {
+			newError = 'Максимальная длина 20 символов';
+		}
+
+		updateFormError('passwordError', newError);
+	};
+
+	const onPasswordBlur = ({ target }) => {
+		if (!checkMinLength(target.value, 6)) {
+			updateFormError('passwordError', 'Минимальная длина 6 символов');
+		}
+	};
+
+	const onConfirmPasswordChange = ({ target }) => {
+		updateFormData('confirmPassword', target.value);
+		updateFormError('confirmPasswordError', null);
+	};
+
+	const onConfirmPasswordBlur = ({ target }) => {
+		if (password !== target.value) {
+			updateFormError('confirmPasswordError', 'Пароли не совпадают');
+		}
+	};
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		let error = false;
+		if (!email) {
+			error = true;
+			updateFormError('emailError', 'Обязательное поле');
+		}
+		if (!password) {
+			error = true;
+			updateFormError('passwordError', 'Обязательное поле');
+		}
+		if (!confirmPassword) {
+			error = true;
+			updateFormError('confirmPasswordError', 'Обязательное поле');
+		}
+		if (error) {
+			return;
+		}
+		const formData = {
+			email: email.toLowerCase(),
+			password,
+			confirmPassword,
+		};
+		sendForm(formData);
+	};
+
+	return (
+		<form className={styles.form} onSubmit={onSubmit}>
+			<input
+				autoComplete="email"
+				placeholder="Ваш Email"
+				type="text"
+				name="email"
+				id="email"
+				value={email}
+				onChange={onEmailChange}
+				onBlur={onEmailBlur}
+			/>
+			{emailError ? <span>{emailError}</span> : <span />}
+			<input
+				autoComplete="new-password"
+				placeholder="Введите пароль"
+				type="password"
+				name="password"
+				id="password"
+				value={password}
+				onChange={onPasswordChange}
+				onBlur={onPasswordBlur}
+			/>
+			{passwordError ? <span>{passwordError}</span> : <span />}
+			<input
+				autoComplete="new-password"
+				placeholder="Повторите пароль"
+				type="password"
+				name="confirmPassword"
+				id="confirmPassword"
+				value={confirmPassword}
+				onChange={onConfirmPasswordChange}
+				onBlur={onConfirmPasswordBlur}
+			/>
+			{confirmPasswordError ? <span>{confirmPasswordError}</span> : <span />}
+			<input
+				disabled={emailError || passwordError || confirmPasswordError}
+				type="submit"
+				value="Зарегистрироваться"
+			/>
+		</form>
+	);
+};
